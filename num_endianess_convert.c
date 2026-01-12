44faff4ea13748deba70baca6013e79370131d9c
Yuman0627 <1521337603@qq.com>
2026-01-12 19:52:17 +0800

    主机字节符和网络字节符的转换。

---
+++ num_endianess_convert.c
@@ -0,0 +1,24 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+
+int main(int argc, char *argv[])
+{
+    //声明两个端口号
+    unsigned short int local_num=0x1f,network_num=0;
+
+    network_num=htons(local_num);
+    printf("将主机字节序无符号整数0x%hX转换为网络字节序0x%hX",local_num,network_num);
+
+    local_num=ntohs(network_num);
+    printf("将网络字节序无符号整数0x%hX转换为主机字节序0x%hX",network_num,local_num);
+
+    return 0; 
+
+}
